a5a11a35da4ee45ca7f916c73e583471eefb9b02
EDUB <35309993+eemdub@users.noreply.github.com>
2021-06-20 23:34:52 -0500

    reverted to old api

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Nrwq_8XnS7yfNLnt_0515WPyFFfDFJ0i4U2bx3VaJM4/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyB0gb37qTBxie1yApXdXAM4SwA77q5jAXY';
+var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
